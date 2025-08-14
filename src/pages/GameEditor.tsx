7f991f1ba2a67bdaf2c2089fffdbc28116bc9189
Jonathan NZAU MAKOSO <jonathannzaumakoso@MacBook-Air-de-Jonathan.local>
2025-08-14 13:20:50 +0200

    refactor: disable drag & drop functionality across components

--- src/pages/GameEditor.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import GameEditorLayout from '../components/GameEditor/GameEditorLayout';
-
-const GameEditor: React.FC = () => {
-  return <GameEditorLayout />;
-};
-
-export default GameEditor;
