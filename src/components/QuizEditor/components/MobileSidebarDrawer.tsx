790cd8d6196b1768e9be91a7ace4aec719776822
Niakdashit <jonanzau13@gmail.com>
2025-09-09 15:48:04 +0200

    fix: init campaign in mobile drawer

--- src/components/QuizEditor/components/MobileSidebarDrawer.tsx
+++ src/components/QuizEditor/components/MobileSidebarDrawer.tsx
@@ -45,6 +45,7 @@ const MobileSidebarDrawer: React.FC<MobileSidebarDrawerProps> = ({
   onBackgroundChange,
   onExtractedColorsChange,
   currentBackground,
+  campaignConfig,
   elements = [],
   onElementsChange,
   selectedElement,
@@ -56,6 +57,11 @@ const MobileSidebarDrawer: React.FC<MobileSidebarDrawerProps> = ({
 }) => {
   const campaign = useEditorStore((s: any) => s.campaign);
   const setCampaign = useEditorStore((s: any) => s.setCampaign);
+  useEffect(() => {
+    if (!campaign && campaignConfig) {
+      setCampaign(campaignConfig);
+    }
+  }, [campaign, campaignConfig, setCampaign]);
   const [activeTab, setActiveTab] = useState<string>('elements');
   const [isMinimized, setIsMinimized] = useState(true);
 
